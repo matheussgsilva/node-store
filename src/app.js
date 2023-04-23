6523c1772b7d7b396e4843dac0e69db6f7279571
Matheus Silva <matheussgsilva@hotmail.com>
2023-04-23 19:17:10 -0300

    Change DB password

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ const app = express();
 const router = express.Router();
 
 // Connect to DB
-mongoose.connect('mongodb+srv://matheussgsilva:Bg4h03DEZHFWIjEu@cluster0.irwcn1k.mongodb.net/test');
+mongoose.connect('mongodb+srv://matheussgsilva:yTGjJwY9g6Ukhn86@cluster0.irwcn1k.mongodb.net/test');
 
 // Load Models
 const Product = require('./models/product');
