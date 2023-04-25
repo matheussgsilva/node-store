8a370f587cca3492d5e541e50703d22ad1e6e75c
Matheus Silva <matheussgsilva@hotmail.com>
2023-04-25 19:01:51 -0300

    Create the email service

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
 global.SALT_KEY = 'f5b99242-6504-4ca3-90f2-05e78e5761ef';
-global.EMAIL_TMPL = '<strong>{0}</strong>';
+global.EMAIL_TMPL = 'Olá <strong>{0}</strong>, seja bem vindo à Node Store! ';
 
 module.exports = {
     conectionString: 'mongodb+srv://matheussgsilva:yTGjJwY9g6Ukhn86@cluster0.irwcn1k.mongodb.net/test',
-    sendgridKey: 'TBD',
+    sendgridKey: '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',
     containerConnectionString: 'TBD',
 }
